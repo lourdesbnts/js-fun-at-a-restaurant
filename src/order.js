d16dcc90ea506fd64b9656e8fe7deec749274c73
Lourdes Benites <lourdesben95@gmail.com>
2021-12-04 16:30:12 -0700

    Add order test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(newOrder, deliveryOrders) {
+  if (deliveryOrders.length <= 2) {
+    return deliveryOrders.push(newOrder)
+  }
+ }
+
+function refundOrder(orderNum, orderList) {
+  for (var i = 0; i < orderList.length; i++) {
+
+    if (orderList[i].orderNumber === orderNum) {
+      return orderList.splice(i,1)
+    }
+  }
+}
+
+function listItems(orders) {
+  var temp = []
+  for (var i = 0; i < orders.length; i++){
+    // return orders[i].item
+    temp.push(orders[i].item)
+
+  }
+  return temp.join(", ")
+}
+
+function searchOrder(orders, orderItem) {
+  var temp  = [];
+  for (var i = 0; i < orders.length; i++) {
+    temp.push(orders[i].item)
+  }
+if(temp.includes(orderItem)){
+  return true
+} else {
+  return false
+ }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   refundOrder,
+   listItems,
+   searchOrder
 }
