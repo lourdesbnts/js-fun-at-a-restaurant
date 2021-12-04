362b8d7ddfc8136e0babbdfe06a9bdb4fd2d648f
Lourdes Benites <lourdesben95@gmail.com>
2021-12-03 21:31:43 -0700

    Add meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,50 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`
 }
 
+function createMenuItem(foodName, mealPrice, meal) {
+  var menuOption = {
+    name: foodName,
+    price: mealPrice,
+    type: meal,
+  }
+  return menuOption;
+}
+
+// var ingredients = [];
+
+function addIngredients(topping1, ingredients) {
+  for(var i=0; i < ingredients.length; i++) {
+  if (topping1 === ingredients[i]) {
+    return
+  }
+  //return ingredients
+}
+//
+    return ingredients.push(topping1)
+  }
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return (price * .9)
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+ var newRecipe = {
+   title: title,
+   ingredients: ingredients,
+   type: menuItemType,
+ }
+ return newRecipe
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
